d1aec5bca9e83e2711b1ef2876a977337e03923f
Luis-Eduardo-Fr <qjy3gHtqmu9B@outlook.com>
2024-03-19 12:49:35 -0300

    att

--- py/p.py
+++ py/p.py
@@ -1,4 +1,5 @@
 import platform
+import time
 
 if platform.system() == 'Linux':
     print("Parece que está utilizando Linux, você é chad!")
@@ -6,3 +7,10 @@ elif platform.system() == 'Windows':
     print("Parece que você está utilizando Windows, você você é sub-chad!")
 else:
     print("Seu sistema não é suportado.")
+
+print("Carregando", end = '')
+for _ in range(3):
+    time.sleep(0.5)
+    print('.', end = '', flush = True)
+    time.sleep(0.5)
+print("Found!")
--- py/radiopy.py
+++ py/radiopy.py
@@ -22,24 +22,20 @@ def verify_os():
     if platform.system() == 'Linux':
         host_platform = 'GNU/LINUX'
         print("Sistema" + Fore.RED + " " + host_platform + " " + Style.RESET_ALL + "detectado, adaptando o programa", end = '', flush = True)
-        print('.', end = '', flush = True)
-        time.sleep(0.5)
-        print('.', end = '', flush = True)
-        time.sleep(0.5)
-        print('.', end = '', flush = True)
-        time.sleep(0.3)
+        for _ in range(3):
+            time.sleep(0.3)
+            print('.', end = '', flush = True)
+            time.sleep(0.3)
         os.system('clear')
         print("RADIOPY by lefds" + Fore.RED + " Linux Edition" + Style.RESET_ALL + " " + version)
         get_info_linux()
     elif platform.system() == 'Windows':
         host_platform = 'WINDOWS'
         print("Sistema" + Fore.BLUE + " " + host_platform + " " + Style.RESET_ALL + "detectado, adaptando o programa", end = '', flush = True)
-        print('.', end = '', flush = True)
-        time.sleep(0.5)
-        print('.', end = '', flush = True)
-        time.sleep(0.5)
-        print('.', end = '', flush = True)
-        time.sleep(0.3)
+        for _ in range(3):
+            time.sleep(0.3)
+            print('.', end = '', flush = True)
+            time.sleep(0.3)
         os.system('cls')
         print("RADIOPY by lefds" + Fore.BLUE + " Windows Edition" + Style.RESET_ALL + " " + version)
         get_info_win()
@@ -51,38 +47,29 @@ def get_info_linux():
     if ui.lower() in ['acustica', 'acústica']:
         radio_name, radio_url = 'Acústica FM 97.7MHz', 'https://live.virtualcast.com.br/acusticacamaqua' 
         print(Fore.YELLOW + "Acessando o banco de dados" + Style.RESET_ALL, end = '', flush=True)
-        time.sleep(0.8)
-        print(".", end = '', flush=True)
-        time.sleep(0.8)
-        print(".", end = '', flush=True)
-        time.sleep(0.8)
-        print(".", end = '', flush=True)
-        time.sleep(0.8)
+        for _ in range(3):
+            time.sleep(0.5)
+            print(".", end = '', flush=True)
+            time.sleep(0.5)
         print(Fore.GREEN + " Encontrada" + Style.RESET_ALL + "!", end = '', flush=True)
         time.sleep(1.5)
         play_radio_linux(radio_name, radio_url)
     elif ui.lower() in ['gaucha', 'gaúcha']: 
         radio_name, radio_url = 'Gaúcha FM 93.7MHz', 'https://liverdgaupoa.rbsdirect.com.br/primary/gaucha_rbs.sdp/chunklist_36c916a3-44bd-4777-ac8e-2805b97d216b.m3u8'
         print(Fore.YELLOW + "Acessando o banco de dados" + Style.RESET_ALL, end = '', flush=True)
-        time.sleep(0.5)
-        print(".", end = '', flush=True)
-        time.sleep(0.5)
-        print(".", end = '', flush=True)
-        time.sleep(0.5)
-        print(".", end = '', flush=True)
-        time.sleep(0.5)
+        for _ in range(3):
+            time.sleep(0.3)
+            print(".", end = '', flush=True)
+            time.sleep(0.3)
         print(Fore.GREEN + " Encontrada!" + Style.RESET_ALL, end = '', flush=True)
-        time.sleep(1)
+        time.sleep(0.7)
         play_radio_linux(radio_name, radio_url)
     else:
         print(Fore.YELLOW + "Acessando o banco de dados" + Style.RESET_ALL, end = '', flush=True)
-        time.sleep(0.3)
-        print(".", end = '', flush=True)
-        time.sleep(0.3)
-        print(".", end = '', flush=True)
-        time.sleep(0.3)
-        print(".", end = '', flush=True)
-        time.sleep(0.3)
+        for _ in range(3):
+            time.sleep(0.3)
+            print(".", end = '', flush=True)
+            time.sleep(0.3)
         loop_input = input(Fore.RED +" Estação não encontrada" + Style.RESET_ALL + "!" + "\nTentar novamente?: ")
         if loop_input in ['s', 'sim', 'y', 'yes' 'claro', 'obv']:
             get_info_linux()
