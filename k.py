03335e1c19832f2989024fe005c49f0f07cb0531
Luis-Eduardo-Fr <qjy3gHtqmu9B@outlook.com>
2024-03-22 18:23:35 -0300

    RPY version ALPHA 0.0.4

--- k.py
+++ k.py
@@ -1,23 +1,32 @@
-import vlc
-import time
+import requests
+import platform
 import os
-from typing import cast
-max_wait = 2
-wait_time = 0
+import sys
+from colorama import Fore, Style
 
-instance = cast(vlc.Instance, vlc.Instance('--quiet --no-xlib'))
-media = instance.media_new('/home/lefds/5.mp4')
-player = instance.media_player_new()
-player.set_media(media)
-player.play()
-
-while player.is_playing:
-    time.sleep(1)
-    #wait_time += 1
-    #if wait_time >= max_wait:
-#        break
-#instance.release()
-#os.kill(player.get_pid(), 9)
+def verification(host_os):
+    try:
+        requests.get('https://www.google.com', timeout=5) # Check Internet connection
+    except Exception:
+        print(Fore.RED + "Connect your PC to the internet before continuing." + Style.RESET_ALL)
+        sys.exit()
     
+    if platform.system() == 'Linux':
+        host_os = 'GNU/LINUX' # Assign value to host_os
+        version = ''
+        print("RADIOPY by lefds" + Fore.RED + " Linux Edition" + Style.RESET_ALL + " " + version) # Assuming version is defined
+        if os.getuid() == 0:
+            print(Fore.RED + "This script should not be run as superuser (root)." + Style.RESET_ALL)
+            sys.exit()
+        if not os.path.exists('/usr/bin/mpv') and not os.path.exists('/usr/local/bin/mpv'):
+            print(Fore.RED + "ERROR: MPV is not installed." + Style.RESET_ALL)
+            sys.exit()
     
+    # You can add more conditions for other OS types here
+    return host_os
 
+# Assuming station_id is defined
+host_os = verification(None)
+
+if host_os == 'GNU/LINUX':
+    print("Ih mano existe não")
--- py/radiopy.py
+++ py/radiopy.py
@@ -3,51 +3,41 @@ from typing import cast
 import subprocess
 import platform
 import requests
+#import winreg
 import time
+import sys
 import vlc
 import os
 
 
-station = ['acustica', 'gaucha']
-station_name = ['Acústica FM 97.7MHz', 'Gaúcha FM 93.7MHz']
-station_url = ['https://live.virtualcast.com.br/acusticacamaqua', 'https://liverdgaupoa.rbsdirect.com.br/primary/gaucha_rbs.sdp/chunklist_36c916a3-44bd-4777-ac8e-2805b97d216b.m3u8']
 
 
-def verify_net():
+def verification(host_os):
     try:
-        requests.get("https://www.google.com", timeout=5)
+        requests.get('https://www.google.com', timeout=5) # Verifica se a Internet;
     except Exception:
-        print(Fore.RED + "Conecte seu computador a internet antes de continuar.." + Style.RESET_ALL)
-        exit() 
-
-
-
-def verify_os():
-    if platform.system() == 'Linux':
-        host_platform = 'GNU/LINUX'
-        print("Sistema" + Fore.RED + " " + host_platform + " " + Style.RESET_ALL + "detectado, adaptando o programa", end = '', flush = True)
-        for _ in range(3):
-            time.sleep(0.3)
-            print('.', end = '', flush = True)
-            time.sleep(0.3)
-        os.system('clear')
+        print(Fore.RED + "Conecte seu PC a internet à internet antes de continuar.." + Style.RESET_ALL)
+        sys.exit()
+    if platform.system() == 'Linux': # Verifica o sistema do usuário;
+        host_os = 'GNU/LINUX'
         print("RADIOPY by lefds" + Fore.RED + " Linux Edition" + Style.RESET_ALL + " " + version)
-        get_info_linux()
-    elif platform.system() == 'Windows':
-        host_platform = 'WINDOWS'
-        print("Sistema" + Fore.BLUE + " " + host_platform + " " + Style.RESET_ALL + "detectado, adaptando o programa", end = '', flush = True)
-        for _ in range(3):
-            time.sleep(0.3)
-            print('.', end = '', flush = True)
-            time.sleep(0.3)
-        os.system('cls')
-        print("RADIOPY by lefds" + Fore.BLUE + " Windows Edition" + Style.RESET_ALL + " " + version)
-        get_info_win()
+        if os.getuid() == 0:
+            print(Fore.RED + "Este script não deve ser reproduzido como SuperUsuário (ROOT)." + Style.RESET_ALL)
+            sys.exit()
+        if not os.path.exists("/usr/bin/mpv") and not os.path.exists("/usr/local/bin/mpv"):
+            print(Fore.RED + "ERRO: MPV não está instalado." + Style.RESET_ALL)
+            sys.exit()
 
-def get_info_linux():
+
+    return host_os
+        
+
+
+def get_input_linux():
     ui = input("Digite a estação: ")
     while len(ui) == 0:
-        get_info_linux()
+        ui = input("Digite a estação: ")
+
     if ui.lower() in station[0]:
         station_id = 0
         print(Fore.YELLOW + "Acessando o banco de dados" + Style.RESET_ALL, end = '', flush=True)
@@ -57,31 +47,38 @@ def get_info_linux():
             time.sleep(0.2)
         print(Fore.GREEN + " Encontrada" + Style.RESET_ALL + "!", end = '', flush=True)
         time.sleep(0.7)
-        play_radio_linux(station_id)
+        return True, station_id
+
     elif ui.lower() in station[1]: 
         station_id = 1
         print(Fore.YELLOW + "Acessando o banco de dados" + Style.RESET_ALL, end = '', flush=True)
+
         for _ in range(3):
             time.sleep(0.2)
             print(".", end = '', flush=True)
             time.sleep(0.2)
+
         print(Fore.GREEN + " Encontrada!" + Style.RESET_ALL, end = '', flush=True)
         time.sleep(0.7)
-        play_radio_linux(station_id)
+        return True, station_id
     else:
         print(Fore.YELLOW + "Acessando o banco de dados" + Style.RESET_ALL, end = '', flush=True)
+
         for _ in range(3):
             time.sleep(0.3)
             print(".", end = '', flush=True)
             time.sleep(0.3)
+
         loop_input = input(Fore.RED +" Estação não encontrada" + Style.RESET_ALL + "!" + "\nTentar novamente?: ")
+    
         if loop_input in ['s', 'sim', 'y', 'yes' 'claro', 'obv']:
-            get_info_linux()
+            get_input_linux()
+
         elif loop_input in ['n', 'nao', 'não', 'no']:
             print("Tchau!")
+            sys.exit()
             
         
-
 
 def play_radio_linux(station_id):
     try:
@@ -94,13 +91,18 @@ def play_radio_linux(station_id):
         print(Fore.RED + "Houve um erro durante a reprodução da estação, consulte o PODEROSO lefds" + Style.RESET_ALL)
 
 
+#==========================================================================================================================#
+#==========================================================================================================================#
 
-def get_info_win():
+
+def get_input_win():
+
     ui = input("Digite a estação: ")
     while len(ui) == 0:
-        get_info_win()
-    if ui.lower() in ['acustica', 'acústica']:
-        radio_name, radio_url = 'Acústica FM 97.7MHz', 'https://live.virtualcast.com.br/acusticacamaqua' 
+        ui = input("Digite a estação: ")
+
+    if ui.lower() in station[0]:
+        station_id = 0
         print(Fore.YELLOW + "Acessando o banco de dados" + Style.RESET_ALL, end = '', flush=True)
         time.sleep(0.8)
         print(".", end = '', flush=True)
@@ -109,12 +111,26 @@ def get_info_win():
         time.sleep(0.8)
         print(".", end = '', flush=True)
         time.sleep(0.8)
-        print(Fore.GREEN + "Encontrada!" + Style.RESET_ALL, end = '', flush=True)
+        print(Fore.GREEN + " Encontrada!" + Style.RESET_ALL, end = '', flush=True)
         time.sleep(1.5)
-        play_radio_win(radio_name, radio_url)
-    elif ui.lower() in ['gaucha', 'gaúcha']: 
-        radio_name, radio_url = 'Gaúcha FM 93.7MHz', 'https://liverdgaupoa.rbsdirect.com.br/primary/gaucha_rbs.sdp/chunklist_36c916a3-44bd-4777-ac8e-2805b97d216b.m3u8'
-        play_radio_win(radio_name, radio_url)
+        return True, station_id
+                               
+
+    elif ui.lower() in station[1]: 
+        station_id = 1
+        print(Fore.YELLOW + "Acessando o banco de dados" + Style.RESET_ALL, end = '', flush=True)
+        time.sleep(0.8)
+        print(".", end = '', flush=True)
+        time.sleep(0.8)
+        print(".", end = '', flush=True)
+        time.sleep(0.8)
+        print(".", end = '', flush=True)
+        time.sleep(0.8)
+        print(Fore.GREEN + " Encontrada!" + Style.RESET_ALL, end = '', flush=True)
+        time.sleep(1.5)
+        return True, station_id
+
+
     else:
         print(Fore.YELLOW + "Acessando o banco de dados" + Style.RESET_ALL, end = '', flush=True)
         time.sleep(0.3)
@@ -126,19 +142,19 @@ def get_info_win():
         time.sleep(0.3)
         loop_input = input(Fore.RED +" Estação não encontrada" + Style.RESET_ALL + "!" + "\nTentar novamente?: ")
         if loop_input in ['s', 'sim', 'y', 'yes' 'claro', 'obv']:
-            get_info_win()
+            get_input_win()
         elif loop_input in ['n', 'nao', 'não', 'no']:
-            print("Tchau!")
+            print(Fore.YELLOW + "Programa encerrado." + Style.RESET_ALL)
  
 
 
-def play_radio_win(radio_name, radio_url):
+def play_radio_win(station_id):
     os.system('cls')
     instance = cast(vlc.Instance, vlc.Instance('--quiet'))
-    media = instance.media_new(radio_url)
+    media = instance.media_new(station_url[station_id])
     player = instance.media_player_new()
     player.set_media(media)
-    print("Tocando a " + Fore.YELLOW + radio_name + Style.RESET_ALL + "!")
+    print("Tocando a " + Fore.YELLOW + station_name[station_id] + Style.RESET_ALL + "!")
     try:
         player.play()
         input("Pressione Enter para parar a reprodução..")
@@ -150,6 +166,7 @@ def play_radio_win(radio_name, radio_url):
 
 
 
+
 ######################################################################################################
 #
 ##   RadioPY by lefds
@@ -157,20 +174,31 @@ def play_radio_win(radio_name, radio_url):
 ##  ALPHA 0.0.1
 ##  ALPHA 0.0.2 -- Added Net-check.
 ##  ALPHA 0.0.3 -- Changed stations based on if statements, now in their own lists.
+##  ALPHA 0.0.4 -- BIG Changes on code structure, now more human-readable and optimized.
 ##
 #
 
-version = 'ALPHA 0.0.3'
+version = 'ALPHA 0.0.4'
 
-
+station = ['acustica', 'gaucha', 'grenal']
+station_name = ['Acústica FM 97.7MHz', 'Gaúcha FM 93.7MHz', 'Rádio GRENAL 95.9MHz FM']
+station_url = ['https://live.virtualcast.com.br/acusticacamaqua', 'https://liverdgaupoa.rbsdirect.com.br/primary/gaucha_rbs.sdp/chunklist_36c916a3-44bd-4777-ac8e-2805b97d216b.m3u8', ]
 # Iniciando o colorama
 init()
 
-# Verifica se o usuário está conectado a internet
-verify_net()
+# Verifica todas as dependências necessárias
+host_os = verification(None)
 
-# Verifica o Sistema Host
-verify_os()
+if host_os == 'GNU/LINUX':
+    sucess, station_id = get_input_linux()
+    if sucess:
+        play_radio_linux(station_id)
+
+elif host_os == 'WINDOWS':
+    sucess, station_id = get_input_win()
+    if sucess:
+        play_radio_win(station_id)
+
 
 # O resto do código perdura em funções que são chamadas dentro da função mãe (verify_os),
 # refatorações podem (e provavelmente serão) feitas posteriormente...
